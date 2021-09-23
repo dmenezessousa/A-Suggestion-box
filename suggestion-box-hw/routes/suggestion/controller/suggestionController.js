3a57159e81dd41ed534671e3baf7aaabe50c8bc6
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-09-23 15:42:30 -0400

    Added CRUG

--- suggestion-box-hw/routes/suggestion/controller/suggestionController.js
+++ suggestion-box-hw/routes/suggestion/controller/suggestionController.js
@@ -1,5 +1,69 @@
-const Suggestion = require("../model/Suggestion.js");
+const Suggestion = require("../modal/Suggestion.js");
 
 module.exports = {
-    
-}
+    //get = uses .find
+    //GET ALL
+    getAllSuggestions:(body,callback)=>{
+        Suggestion.find(body,(err,foundAllSuggestions)=>{
+            if(err){
+                callback(err,null);
+            }else{
+                callback(null, foundAllSuggestions);
+            }
+        });
+    },
+
+    //GET ONE
+    getOneSuggestion:(id,body,callback)=>{
+        Suggestion.find(id,body,(err,foundSuggestion)=>{
+            if(err){
+                callback(err,null);
+            }else{
+                callback(null, foundSuggestion);
+            }
+        });
+    },
+
+    //POST = uses .save
+    createSuggestion:(body,callback)=>{
+        //create a new Array of Objects
+        const createdSuggestion = new Suggestion({
+            tittle: body.tittle,
+            author: body.author,
+            suggestion: body.suggestion,
+            likes: body.likes,
+            anonymous: body.anonymous,
+            timeCreated:  body.timeCreated
+        });
+
+        createdSuggestion.save((err,savedSuggestion)=>{
+            if(err){
+                callback(err,null);
+            }else{
+                callback(null, savedSuggestion);
+            }
+        });
+    },
+
+    //Update = uses.findByIdAndUpdate
+    updateSuggestion:(id,body,callback)=>{
+        Suggestion.findByIdAndUpdate(id,body,{new:true},(err,updateSuggestion)=>{
+            if(err){
+                callback(err,null);
+            }else{
+                callback(null, updateSuggestion);
+            }
+        });
+    },
+
+    //DELETE = uses .findByIdAndDelete
+    deleteSuggestion:(id,callback)=>{
+        Suggestion.findByIdAndDelete(id,(err,deletedSuggestion)=>{
+            if(err){
+                callback(err,null);
+            }else{
+                callback(null, deletedSuggestion);
+            };
+        });
+    }
+};
--- suggestion-box-hw/routes/suggestion/suggestionRouter.js
+++ suggestion-box-hw/routes/suggestion/suggestionRouter.js
@@ -1,15 +1,71 @@
 const express = require("express");
 const router = express.Router();
+const suggestionController = require("./controller/suggestionController");
 
+//get all request
+router.get("/",(req,res)=>{
+    suggestionController.getAllSuggestions(req.body,(err, foundSuggestions)=>{
+        if(err){
+            res.status(500)
+            .json({message: "Something went wrong!", error: err.message});
+        }else{
+            res.json({message: "Success", foundSuggestions});
+        }
+    });
+});
 
+//get one request
+router.get("/get-suggestion-by-id/:id",(req,res)=>{
+    const {id} = req.params;
+    suggestionController.getOneSuggestion(id, (err, deleteSuggestion)=>{
+        if(err){
+            res
+            .status(500)
+            .json({message:"Something went wrong!", error: err.message});
+        }else{
+            res.json({message: "Success", deleteSuggestion});
+        }
+    });
+});
 
+//POST request 
+router.post("/create-suggestion",(err,res)=>{
+    suggestionController.createSuggestion(req.body,(err,savedSuggestion)=>{
+        if(err){
+            res
+            .status(500)
+            .json({message:"Something went wrong!", error: err.message});
+        }else{
+            res.json({message: "Success", savedSuggestion});
+        }
+    });
+});
 
+//DELETE request
+router.delete("/delete-suggestion-by-id/:id",(req,res)=>{
+    const {id} = req.params;
+    suggestionController.deleteSuggestion(id, (err, deleteSuggestion)=>{
+        if(err){
+            res
+            .status(500)
+            .json({message:"Something went wrong!", error: err.message});
+        }else{
+            res.json({message: "Success", deleteSuggestion});
+        }
+    });
+});
 
-
-
-
-
-
-
+//UPDATE request
+router.put("/update-suggestion-by-id/:id",(req,res)=>{
+    const {id} = req.params;
+    suggestionController.updateSuggestion(id,req.body,(err, updateSuggestion)=>{
+        if(err){
+            res.status(500)
+            .json({message:"Something went wrong!", error: err.message});
+        }else{
+            res.json({message: "success", updateSuggestion });
+        }
+    });
+});
 
 module.exports = router;
